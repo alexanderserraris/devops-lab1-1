d25c519ad1498956bc8798f26a8e15fea0d577c2
Alexander Serraris <alexander.cs.dev@gmail.com>
2025-03-04 15:43:11 +0100

    fix all services for mongo and industry standards

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,6 +1,8 @@
 import os
 from pymongo import MongoClient
 
+from swagger_server.models.student import Student
+
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://mongo:27017")
 MONGO_DB = os.environ.get("MONGO_DB", "student_db")
 
@@ -9,29 +11,34 @@ db = client[MONGO_DB]
 students_collection = db["students"]
 
 
-def add(student=None):
+def add(student: Student) -> int:
     res = students_collection.find_one(
         {"first_name": student.first_name, "last_name": student.last_name}
     )
     if res:
         return "already exists", 409
 
-    doc_id = students_collection.insert_one(student.to_dict()).inserted_id
-    return str(doc_id)  # we need to stringify the ObjectId for valid JSON
+    student.student_id = (
+        students_collection.count_documents({}) + 1
+        if student.student_id is None
+        else student.student_id
+    )
+    students_collection.insert_one(student.to_dict())
+    return student.student_id
 
 
-def get_by_id(student_id=None, subject=None):
-    student = students_collection.find_one({"_id": student_id})
+def get_by_id(student_id: int = None, subject=None) -> Student:
+    student = students_collection.find_one({"student_id": int(student_id)})
     if not student:
         return "not found", 404
-    student["student_id"] = student_id
-    print(student)
-    return student
+
+    return Student.from_dict(student)
 
 
-def delete(student_id=None):
-    student = students_collection.find_one({"_id": student_id})
+def delete(student_id: int = None) -> int:
+    student_id = int(student_id)
+    student = students_collection.find_one({"student_id": student_id})
     if not student:
         return "not found", 404
-    students_collection.delete_one({"_id": student_id})
+    students_collection.delete_one({"student_id": student_id})
     return student_id
