7edfbeafcbdf120f1160953635501c8b37203abb
Alexander Serraris <alexander.cs.dev@gmail.com>
2025-03-04 14:59:35 +0100

    fix bug with using db instead of collection and fix right methods

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,9 +1,4 @@
 import os
-import tempfile
-from functools import reduce
-
-from tinydb import TinyDB, Query
-
 from pymongo import MongoClient
 
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://mongo:27017")
@@ -11,22 +6,22 @@ MONGO_DB = os.environ.get("MONGO_DB", "student_db")
 
 client = MongoClient(MONGO_URI)
 db = client[MONGO_DB]
+students_collection = db["students"]
 
 
 def add(student=None):
-    res = db.find_one(
+    res = students_collection.find_one(
         {"first_name": student.first_name, "last_name": student.last_name}
     )
     if res:
         return "already exists", 409
 
-    doc_id = db.insert(student.to_dict())
-    student.student_id = doc_id
-    return student.student_id
+    doc_id = students_collection.insert_one(student.to_dict()).inserted_id
+    return str(doc_id)  # we need to stringify the ObjectId for valid JSON
 
 
 def get_by_id(student_id=None, subject=None):
-    student = db.find_one({"_id": student_id})
+    student = students_collection.find_one({"_id": student_id})
     if not student:
         return "not found", 404
     student["student_id"] = student_id
@@ -35,8 +30,8 @@ def get_by_id(student_id=None, subject=None):
 
 
 def delete(student_id=None):
-    student = db.find_one({"_id": student_id})
+    student = students_collection.find_one({"_id": student_id})
     if not student:
         return "not found", 404
-    db.delete({"_id": student_id})
+    students_collection.delete_one({"_id": student_id})
     return student_id
